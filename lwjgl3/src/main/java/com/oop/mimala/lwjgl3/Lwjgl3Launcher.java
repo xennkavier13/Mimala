07243a814a5b3885a754e97bce7b2fc07c2e8866
Kenn Xavier <kennxavier15@gmail.com>
2025-03-12 23:02:00 +0800

    "Added comment //MIMALA LAUNCHER"

--- lwjgl3/src/main/java/com/oop/mimala/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/oop/mimala/lwjgl3/Lwjgl3Launcher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.oop.mimala.Mimala;
 
 /** Launches the desktop (LWJGL3) application. */
+//MIMALA LAUNCHER
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
