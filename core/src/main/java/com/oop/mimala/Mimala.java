c5c8abf544eb7f0ff6b7300b4bd35907a7afb997
Kenn Xavier <kennxavier15@gmail.com>
2025-03-13 12:38:04 +0800

    "tested a few things"

--- core/src/main/java/com/oop/mimala/Mimala.java
+++ core/src/main/java/com/oop/mimala/Mimala.java
@@ -5,33 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Mimala extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture testball;
-
-
+    private Sprite testball;
     private float x, y; //tracks the position of the testball
     private float speed = 200f; //speed in pixels per second
 
-
     private OrthographicCamera camera;
     private Viewport viewport;
-    private final int VIRTUAL_WIDTH = 800;
-    private final int VIRTUAL_HEIGHT = 600;
+    private final int VIRTUAL_WIDTH = 1920;
+    private final int VIRTUAL_HEIGHT = 1080;
 
+
+    //initialize all global variables here
     @Override
     public void create() {
         batch = new SpriteBatch();
-        testball = new Texture("testball.png");
+        testball = new Sprite(new Texture("testball.png"));
         x = 0;
         y = 0;
-
         camera = new OrthographicCamera();
         viewport = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
         viewport.apply();
@@ -58,7 +56,6 @@ public class Mimala extends ApplicationAdapter {
 
     @Override
     public void render() {
-
         //clears the screen everytime render is called
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
@@ -70,7 +67,9 @@ public class Mimala extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
+
         batch.draw(testball, x, y);
+
         batch.end();
     }
 
