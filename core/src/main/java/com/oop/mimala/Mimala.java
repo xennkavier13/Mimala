141afbe96decfe36b6aa809686c4d0648a10a678
Kenn Xavier <kennxavier15@gmail.com>
2025-03-13 19:52:46 +0800

    "Added jumping feature"

--- core/src/main/java/com/oop/mimala/Mimala.java
+++ core/src/main/java/com/oop/mimala/Mimala.java
@@ -1,8 +1,6 @@
 package com.oop.mimala;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -11,72 +9,105 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+
 public class Mimala extends ApplicationAdapter {
     private SpriteBatch batch;
     private Sprite testball;
-    private float x, y; //tracks the position of the testball
-    private float speed = 200f; //speed in pixels per second
-
-    private OrthographicCamera camera;
     private Viewport viewport;
-    private final int VIRTUAL_WIDTH = 1920;
-    private final int VIRTUAL_HEIGHT = 1080;
+    private OrthographicCamera camera;
+    private Texture bg; //test
+    //for movement
+    private float x, y;
+    private float speed = 200f;
+    private boolean isJumping = false;
+    private float jumpHeight = 50f;
+    private float jumpSpeed = 300f;
 
+
+    //Game Resolution
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
 
     //initialize all global variables here
     @Override
     public void create() {
         batch = new SpriteBatch();
         testball = new Sprite(new Texture("testball.png"));
-        x = 0;
-        y = 0;
         camera = new OrthographicCamera();
-        viewport = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
+        viewport = new FitViewport(WIDTH, HEIGHT, camera);
         viewport.apply();
 
-        camera.position.set(VIRTUAL_WIDTH / 2f, VIRTUAL_HEIGHT / 2f, 0);
+        camera.position.set(WIDTH / 2f, HEIGHT / 2f, 0);
         camera.update();
+
+
+        bg = new Texture("bg.png");
     }
 
-    public void movementInput(float delta) {
-        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-            y += speed * delta;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)) {
-            y += -(speed) * delta;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            x += -(speed) * delta;
-        }
+    public void input(float delta) {
+//        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+//            y += speed * delta;
+//        }
+//        if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+//            y += -speed * delta;
+//        }
         if(Gdx.input.isKeyPressed(Input.Keys.D)) {
             x += speed * delta;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x += -speed * delta;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && !isJumping) {
+            isJumping = true;
+        }
+
+        if(isJumping) {
+            y += jumpSpeed * delta;
+
+            if(y >= jumpHeight) {
+                isJumping = false;
+            }
+
+        } else {
+            if (y > 0) {
+                y -= jumpSpeed * delta;
+            } else {
+                y = 0;
+            }
         }
 
     }
 
     @Override
     public void render() {
-        //clears the screen everytime render is called
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
-        //delta makes it less laggy when moving
         float delta = Gdx.graphics.getDeltaTime();
-        movementInput(delta);
+        input(delta);
 
+        // ✅ Corrected camera position update
+        camera.position.set(x + testball.getWidth() / 2f, y + testball.getHeight() / 2f, 0);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
+        batch.draw(bg, 0,0, WIDTH, HEIGHT);
 
-        batch.draw(testball, x, y);
-
+        testball.setPosition(x, y);
+        testball.draw(batch);
         batch.end();
     }
 
-
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        bg.dispose();
+        testball.getTexture().dispose();
     }
 
 }
--- lwjgl3/src/main/java/com/oop/mimala/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/oop/mimala/lwjgl3/Lwjgl3Launcher.java
@@ -14,22 +14,22 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3Application createApplication() {
         return new Lwjgl3Application(new Mimala(), getDefaultConfiguration());
+
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
         configuration.setTitle("Mimala");
 
+        int screenWidth = Lwjgl3ApplicationConfiguration.getDisplayMode().width;
+        int screenHeight = Lwjgl3ApplicationConfiguration.getDisplayMode().height - 80;
+
+        configuration.setWindowedMode(screenWidth, screenHeight);
+        configuration.setMaximized(true);
         configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
-        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+
         return configuration;
     }
+
 }
